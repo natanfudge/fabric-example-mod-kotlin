dfcc029c91454fd9d157c30f524f23c1a0e99ed5
natanfudge <natan.lifsiz@gmail.com>
2019-07-27 11:36:27 +0300

    first commit

---
+++ src/main/kotlin/net/fabricmc/example/ExampleMod.kt
@@ -0,0 +1,12 @@
+package net.fabricmc.example
+
+
+@Suppress("unused")
+fun init() {
+    // This code runs as soon as Minecraft is in a mod-load-ready state.
+    // However, some things (like resources) may still be uninitialized.
+    // Proceed with mild caution.
+
+    println("Hello Fabric world!")
+}
+
